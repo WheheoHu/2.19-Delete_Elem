783fdbd5f8ccd065dedc2cd5362f4f44e6aad4a6
文昊 胡 <hwh19981124@hotmail.com>
2018-09-30 09:13:37 +0800

    FrameWork

---
+++ 2.19 Delete_Elem/MainBin.cpp
@@ -0,0 +1,25 @@
+//2.19已知线性表中的元素以值递增有序排列，并以单链表作存储结构。
+//试写一高效的算法，删除表中所有值大于mink且小于maxk的元素（若表中存在这样的元素），
+//同时释放被删结点空间，并分析你的算法的时间复杂度
+//（注意，mink和maxk是给定的两个参变量，它们的值可以和表中的元素相同，也可以不同）。 
+
+const int mink = 10;
+const int maxk = 15;
+const int SIZEOFLIST = 30;
+
+#include <iostream>
+#include "linearlinkedlist.h"
+
+int main() {
+	Linear_Linked_List<int> LLList(0);
+	for (int i = 0; i <SIZEOFLIST ; i++)
+	{
+		LLList.ListInsert(i + 2, i + 1);
+	}
+	for (int i = 0; i < LLList.ListLength(); i++)
+	{
+		std::cout << LLList.GetElem(i + 1) << std::endl;
+	}
+	system("pause");
+	return 0;
+}
