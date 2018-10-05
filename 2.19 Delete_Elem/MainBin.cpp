ac4bb77959df43e978f110f62ec644063a446730
文昊 胡 <hwh19981124@hotmail.com>
2018-10-05 17:39:02 +0800

    Finish

--- 2.19 Delete_Elem/MainBin.cpp
+++ 2.19 Delete_Elem/MainBin.cpp
@@ -3,20 +3,23 @@
 //同时释放被删结点空间，并分析你的算法的时间复杂度
 //（注意，mink和maxk是给定的两个参变量，它们的值可以和表中的元素相同，也可以不同）。 
 
-const int mink = 10;
-const int maxk = 15;
+const int mink = 1;
+const int maxk = 25;
 const int SIZEOFLIST = 30;
 
 #include <iostream>
 #include "linearlinkedlist.h"
 
+void Delete_elem(Linear_Linked_List<int> &LLList, int min, int max);
+
 int main() {
 	Linear_Linked_List<int> LLList(0);
-	for (int i = 0; i <SIZEOFLIST ; i++)
+	for (int i = 0; i < SIZEOFLIST; i++)
 	{
 		LLList.ListInsert(i + 2, i + 1);
 	}
 	//LLList.ListDelete(LLList.ListLength());
+
 	Delete_elem(LLList, mink, maxk);
 
 	for (int i = 0; i < LLList.ListLength(); i++)
@@ -25,4 +28,23 @@ int main() {
 	}
 	system("pause");
 	return 0;
+}
+
+void Delete_elem(Linear_Linked_List<int>& LLList, int min, int max)
+{
+	Node<int> *q = LLList.HeadPoint;
+	Node<int> *temp = NULL;
+	while (q!=NULL)
+	{
+		if (q->next!=NULL&&q->next->data>min&&q->next->data<max)
+		{
+			temp = q->next;
+			q ->next= temp->next;
+			delete temp;
+		}
+		else
+		{
+			q = q->next;
+		}
+	}
 }
